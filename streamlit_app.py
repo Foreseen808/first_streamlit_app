248123feff5cf5257f836b614c4630778fb27ea1
Foreseen808 <88725695+Foreseen808@users.noreply.github.com>
2022-04-21 14:13:21 +0300

    Update streamlit_app.py
    added an interactive widget to allow users to pick the fruits for the smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# pick list to pick up fruit you want to include
+stream.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.title("My Parents New Healthy Diner")
 
