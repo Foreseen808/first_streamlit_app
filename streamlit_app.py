feaf607bf1de95d900b2eb271b3f74f4e4a65a94
Foreseen808 <88725695+Foreseen808@users.noreply.github.com>
2022-04-21 14:14:48 +0300

    Update streamlit_app.py
    changed order

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# pick list to pick up fruit you want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -15,4 +14,6 @@ streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Egg")                 
                  
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# pick list to pick up fruit you want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
