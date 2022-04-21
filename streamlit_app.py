74facb6f52d8518ded0de8f03cccf0fc8c4a93f7
Foreseen808 <88725695+Foreseen808@users.noreply.github.com>
2022-04-21 14:18:34 +0300

    Update streamlit_app.py
    saved fruits selected into a variable and then used said variable to pull rows from the full data set

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,6 @@ streamlit.text("Hard-Boiled Free-Range Egg")
                  
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # pick list to pick up fruit you want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
